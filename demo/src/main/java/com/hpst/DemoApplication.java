5a4b655e383a9f9c9a71e0ccccae829c8eb67626
root <root@hpst-Inspiron-5559>
2019-06-09 19:40:32 +0530

    In Memory authentication methods

--- demo/src/main/java/com/hpst/DemoApplication.java
+++ demo/src/main/java/com/hpst/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.hpst;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +25,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +47,8 @@ public class DemoApplication {
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
 	
 	@Bean
 	PasswordEncoder encoder() {
@@ -52,24 +58,36 @@ class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		log.info("***************configure -start*****************");
-		
-		auth.inMemoryAuthentication().withUser("hpst").password("hpst").authorities("ADMIN");
+		//auth.inMemoryAuthentication().withUser("hpst").password("hpst").authorities("ADMIN");
+		auth.userDetailsService(inMemoryUserDetailManager());
 		log.info("***************configure - end*****************");
+	}
+
+	
+	@Bean
+	public UserDetailsService inMemoryUserDetailManager() {
+		return new InMemoryUserDetailsManager(customUserDetailService.getUserDetails());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("***************configure http-start********* s********");
-		http.formLogin().and().httpBasic();
+	http.authorizeRequests().mvcMatchers("/").authenticated()
+							.mvcMatchers("/hpst").authenticated()
+							.and().formLogin().and().httpBasic();
+//			.and()
+//			.authorizeRequests()
+//			.mvcMatchers("/").permitAll()
+//			.mvcMatchers("/")			
+//			;
+//		http.authorizeRequests().and().formLogin().permitAll();
+//		.mvcMatchers("/").hasRole("ADMIN")
+//		.mvcMatchers("/root").hasRole("ADMIN")
+//		.mvcMatchers("/a").access("hasRole('ADMIN')")
 		
-		http.authorizeRequests()
-		.mvcMatchers("/root").hasRole("ROLE_ADMIN")
-		.mvcMatchers("/a").access("hasRole('ROLE_ADMIN')")
-		.mvcMatchers("/b").access("has");
-		
-		
-		log.info("***************configure http- end*****************");
+	log.info("***************configure http- end*****************");
 	}
+	
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -91,6 +109,10 @@ class CustomUserDetailService implements UserDetailsService{
 	public CustomUserDetailService() {
 		this.detailsMap.put("harpal", new CustomUser("harpal", "password", true, "USER"));
 		this.detailsMap.put("satveer", new CustomUser("satveer", "password", true, "USER", "ADMIN"));
+	}
+	
+	public Collection<UserDetails> getUserDetails() {
+		return detailsMap.values();
 	}
 	
 	@Override
@@ -162,8 +184,8 @@ class CustomUser implements UserDetails{
 class AppController{
 	
 	@GetMapping("/index")
-	public String getHome(){
-		return "indexs";
+	public String getHome(Principal principle){
+		return "indexs"+principle.getName();
 	}
 }
 
